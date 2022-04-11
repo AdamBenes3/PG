4b5f63f0a16992ec65ee5313142a91ea5ff9e2f9
AdamBenes3 <ad.benes@gmail.com>
2022-04-11 08:35:51 +0200

    nefunguje xd zase

--- Lady/Checkers.py
+++ Lady/Checkers.py
@@ -14,6 +14,7 @@ figureDrawer = Turtle(visible=False)
 moving = None
 redOnmove = False
 options = []
+thisFigureJump = -1
 
 def getCornetoIndex(index):
     x = index%8
@@ -37,7 +38,7 @@ def drawFigures():
     figureDrawer.clear()
     for fig in figuresRed:
         figureDrawer.penup()
-        figureDrawer.goto(fig.x*size-width/2 + size/2,height/2 - fig.y*size - size)
+        figureDrawer.goto(fig.x*size-width/2 + size/2, height/2 - fig.y*size - size)
         figureDrawer.pendown()
         figureDrawer.begin_fill()
         figureDrawer.fillcolor("red")
@@ -45,7 +46,7 @@ def drawFigures():
         figureDrawer.end_fill()
     for fig in figuresBlue:
         figureDrawer.penup()
-        figureDrawer.goto(fig.x*size-width/2 + size/2,height/2 - fig.y*size - size)
+        figureDrawer.goto(fig.x*size-width/2 + size/2, height/2 - fig.y*size - size)
         figureDrawer.pendown()
         figureDrawer.begin_fill()
         figureDrawer.fillcolor("blue")
@@ -53,7 +54,7 @@ def drawFigures():
         figureDrawer.end_fill()
     if moving != None:
         figureDrawer.penup()
-        figureDrawer.goto(moving.x*size-width/2 + size/2,height/2 - moving.y*size - size)
+        figureDrawer.goto(moving.x*size-width/2 + size/2, height/2 - moving.y*size - size)
         figureDrawer.pendown()
         figureDrawer.begin_fill()
         figureDrawer.fillcolor("grey")
@@ -61,7 +62,7 @@ def drawFigures():
         figureDrawer.end_fill()   
     for option in options:
         figureDrawer.penup()
-        figureDrawer.goto(option.x*size-width/2 + size/2,height/2 - option.y*size - size)
+        figureDrawer.goto(option.x*size-width/2 + size/2, height/2 - option.y*size - size)
         figureDrawer.pendown()
         figureDrawer.begin_fill()
         figureDrawer.fillcolor("white")
@@ -69,10 +70,10 @@ def drawFigures():
         figureDrawer.end_fill()      
        
 def click(x,y):
-    global moving, redOnmove, options
+    global moving, redOnmove, options, thisFigureJump
     x = floor(x + width/2, width/8) / (width/8)
     y = floor(height/2 - y, height/8) / (width/8)
-    if moving == None:
+    if moving == None: #and thisFigureJump == -1:
         if redOnmove:
             if vector(x,y) in figuresRed:
                 moving = vector(x,y)
@@ -84,22 +85,26 @@ def click(x,y):
     else:
         if moving == vector(x,y):
             moving = None
+            thisFigureJump = -1
         elif isPlayabel(moving, vector(x,y)):
             if redOnmove:
                 figuresRed.remove(moving)
                 figuresRed.append(vector(x,y))
+                thisFigureJump = vector(x,y)
                 #last = moving
                 #for row in range(moving.y+1,y,2):
                 #    pass
             else:
                 figuresBlue.remove(moving)
                 figuresBlue.append(vector(x,y))
+                thisFigureJump = vector(x,y)
             moving = None
             redOnmove = not redOnmove
         options = []
     drawFigures()
+    
 def createOptions(moving):
-    global options, redOnmove
+    global options, redOnmove, thisFigureJump
     options = []
     jumpOptions = []
     if(not redOnmove):
@@ -107,14 +112,11 @@ def createOptions(moving):
             options.append(vector(moving.x + 1, moving.y - 1))
         if isEmpty(vector(moving.x - 1, moving.y - 1)):
             options.append(vector(moving.x - 1, moving.y - 1))
-        jumpOptions = canJump(moving)
-        while len(jumpOptions) > 0:
-            help = []
-            for jump in jumpOptions:
-                options.append(jump)
-                for jump2 in canJump(jump):
-                    help.append(jump2)
-            jumpOptions = help
+        #if thisFigureJump != -1:
+        jumpOptions.append(canJump(moving))
+        for jump in jumpOptions:
+            options.append(jump)
+        print(options)
     else:
         if isEmpty(vector(moving.x + 1, moving.y + 1)):
             options.append(vector(moving.x + 1, moving.y + 1))
@@ -128,6 +130,7 @@ def createOptions(moving):
                 for jump2 in canJump(jump):
                     help.append(jump2)
             jumpOptions = help
+    thisFigureJump = -1
     
 def isPlayabel(moving, target):
     global redOnmove, options
@@ -141,6 +144,7 @@ def canJump(moving):
             jumpOptions.append(vector(moving.x + 2, moving.y - 2))
         if isEmpty(vector(moving.x - 2, moving.y - 2)) and vector(moving.x - 1, moving.y - 1) in figuresRed:
             jumpOptions.append(vector(moving.x - 2, moving.y - 2))
+        print(jumpOptions)
     else:
         if isEmpty(vector(moving.x + 2, moving.y + 2)) and vector(moving.x + 1, moving.y + 1) in figuresBlue:
             jumpOptions.append(vector(moving.x + 2, moving.y + 2))
